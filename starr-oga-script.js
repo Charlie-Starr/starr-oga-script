a781752f95776d542edefc0c0bd2b0e4e5d9e1e9
Charlie-Starr <danielkanyima9@gmail.com>
2025-09-05 13:36:04 +0100

    Update starr-oga-script.js

--- starr-oga-script.js
+++ starr-oga-script.js
@@ -18,7 +18,7 @@
     'use strict';
 
     // --- CONFIGURATION ---
-    const VERCEL_BACKEND_URL = 'https://starr-oga-script.vercel.app/api/script.js';
+    const VERCEL_BACKEND_URL = 'https://starr-oga-script.vercel.app/api/starr.js';
     // CSS Selector for the dating site's input text area where you type replies
     const REPLY_INPUT_SELECTOR = '#reply-textarea';
 
@@ -1425,6 +1425,7 @@
     init();
 
 })();
+
 
 
 
